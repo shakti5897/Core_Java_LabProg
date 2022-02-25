7674db034f0afa0aa43be134dd99bcc797f466dc
shakti5897 <100364928+shakti5897@users.noreply.github.com>
2022-02-25 08:10:54 +0530

    Add files via upload

---
+++ Exercise2.java
@@ -0,0 +1,16 @@
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		        String s1 = "palak";
+		        System.out.print(s1+"|");
+		        for(int i=s1.length()-1;i>=0;i--)
+		            {
+		                System.out.print(s1.charAt(i));
+		            }
+		    }
+
+	}
+
+
